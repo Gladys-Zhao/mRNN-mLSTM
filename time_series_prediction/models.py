23499f237ea8b0f68c96f756fbf0f4028836e64c
Gladys-Zhao <62365420+Gladys-Zhao@users.noreply.github.com>
2021-01-06 23:21:46 +0800

    Update models.py

--- time_series_prediction/models.py
+++ time_series_prediction/models.py
@@ -1,8 +1,11 @@
 #Copyright (C) 2020. Huawei Technologies Co., Ltd. All rights reserved.
 
-#This program is free software; you can redistribute it and/or modify it under the terms of the BSD 3-Clause License.
+#This program is free software; you can redistribute it and/or modify it
+# under the terms of the BSD 3-Clause License.
 
-#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD 3-Clause License for more details.
+#This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the BSD 3-Clause License for more details.
 
 import torch
 import torch.nn as nn
@@ -10,8 +13,9 @@ import torch.nn.functional as F
 from torch.nn.parameter import Parameter
 import layers
 
-# RNN model for time series prediction
+
 class RNN(nn.Module):
+    """RNN model for time series prediction"""
     def __init__(self, input_size, hidden_size, output_size):
         super(RNN, self).__init__()
         self.input_size = input_size
@@ -20,158 +24,181 @@ class RNN(nn.Module):
         self.rnn = nn.RNN(self.input_size, self.hidden_size)
         self.hidden2output = nn.Linear(self.hidden_size, output_size)
 
-    def forward(self, input_y, h=None):
+    def forward(self, input_y, hidden_state=None):
         samples = input_y
-        rnn_out, last_rnn_hidden = self.rnn(samples, h)
+        rnn_out, last_rnn_hidden = self.rnn(samples, hidden_state)
         output = self.hidden2output(rnn_out.view(-1, self.hidden_size))
-        return output.view(samples.shape[0], samples.shape[1], self.output_size), last_rnn_hidden
+        return output.view(samples.shape[0], samples.shape[1],
+                           self.output_size), \
+               last_rnn_hidden
 
-# LSTM model for time series prediction
+
 class LSTM(nn.Module):
+    """LSTM model for time series prediction"""
     def __init__(self, input_size, hidden_size, output_size):
         super(LSTM, self).__init__()
         self.in_size = input_size
         self.h_size = hidden_size
         self.out_size = output_size
-        self.lstmcell = nn.LSTMCell(input_size, hidden_size)
+        self.lstm_cell = nn.LSTMCell(input_size, hidden_size)
         self.output = nn.Linear(hidden_size, output_size)
 
-    def forward(self, input, hx=None):
-        time_steps = input.shape[0]
-        batch_size = input.shape[1]
+    def forward(self, inputs, hidden_state=None):
+        time_steps = inputs.shape[0]
+        batch_size = inputs.shape[1]
         outputs = torch.Tensor(time_steps, batch_size, self.out_size)
-
-        for t in range(time_steps):
-            if hx is None:
-                h_0 = torch.zeros(batch_size, self.h_size)
-                c_0 = torch.zeros(batch_size, self.h_size)
-                hx = (h_0, c_0)
-            else:
-                h_0 = hx[0]
-                c_0 = hx[1]
-            h_0, c_0 = self.lstmcell(input[t, :], (h_0, c_0))
-            outputs[t, :] = self.output(h_0)
+        if hidden_state is None:
+            h_0 = torch.zeros(batch_size, self.h_size)
+            c_0 = torch.zeros(batch_size, self.h_size)
+            hidden_state = (h_0, c_0)
+        else:
+            h_0 = hidden_state[0]
+            c_0 = hidden_state[1]
+        for times in range(time_steps):
+            h_0, c_0 = self.lstm_cell(inputs[times, :], (h_0, c_0))
+            outputs[times, :] = self.output(h_0)
         return outputs, (h_0, c_0)
 
-# mRNN with fixed d for time series prediction
-class MRNN_fixD(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, K, bias=True):
-        super(MRNN_fixD, self).__init__()
-        self.K = K
+
+class MRNNFixD(nn.Module):
+    """mRNN with fixed d for time series prediction"""
+    def __init__(self, input_size, hidden_size, output_size, k, bias=True):
+        super(MRNNFixD, self).__init__()
+        self.k = k
         self.input_size = input_size
         self.output_size = output_size
-        self.bd = Parameter(torch.Tensor(torch.zeros(1, input_size)), requires_grad=True)
-        self.lmrnncell = layers.MRNN_fixD_cell(input_size, hidden_size, output_size, K)
+        self.b_d = Parameter(torch.Tensor(torch.zeros(1, input_size)),
+                             requires_grad=True)
+        self.mrnn_cell = layers.MRNNFixDCell(input_size, hidden_size,
+                                             output_size, k)
+    def get_ws(self, d_values):
+        k = self.k
+        weights = [1.] * (k + 1)
+        for i in range(k):
+            weights[k - i - 1] = weights[k - i] * (i - d_values) / (i + 1)
+        return torch.cat(weights[0:k])
 
-    def get_ws(self, d):
-        K = self.K
-        w = [1.] * (K + 1)
-        for i in range(K):
-            w[K - i - 1] = w[K - i] * (i - d) / (i + 1)
-        return torch.cat(w[0:K])
-
-    def get_wd(self, d):
-        w = torch.ones(self.K, 1, d.size(1), dtype=d.dtype, device=d.device)
-        batch_size = w.shape[1]
-        hidden_size = w.shape[2]
+    def get_wd(self, d_value):
+        weights = torch.ones(self.k, 1, d_value.size(1), dtype=d_value.dtype,
+                             device=d_value.device)
+        batch_size = weights.shape[1]
+        hidden_size = weights.shape[2]
         for sample in range(batch_size):
             for hidden in range(hidden_size):
-                w[:, sample, hidden] = self.get_ws(d[0, hidden].view([1]))
-        return w.squeeze(1)
+                weights[:, sample, hidden] = self.get_ws(d_value[0, hidden].
+                                                         view([1]))
+        return weights.squeeze(1)
 
-    def forward(self, inputs, h=None):
+    def forward(self, inputs, hidden_state=None):
         time_steps = inputs.size(0)
-        self.d = 0.5 * F.sigmoid(self.bd)
-        wd = self.get_wd(self.d)
-        for t in range(time_steps):
-            outputs, h = self.lmrnncell(inputs[t, :], wd, h)
-        return outputs, h
+        self.d_matrix = 0.5 * F.sigmoid(self.b_d)
+        weights_d = self.get_wd(self.d_matrix)
+        for times in range(time_steps):
+            outputs, hidden_state = self.mrnn_cell(inputs[times, :], weights_d,
+                                                   hidden_state)
+        return outputs, hidden_state
 
-# mRNN with dynamic d for time series prediction
+
 class MRNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, K, bias=True):
+    """mRNN with dynamic d for time series prediction"""
+    def __init__(self, input_size, hidden_size, output_size, k, bias=True):
         super(MRNN, self).__init__()
-        self.K = K
+        self.k = k
         self.input_size = input_size
         self.output_size = output_size
-        self.lmrnncell = layers.MRNN_cell(input_size, hidden_size, output_size, K)
+        self.mrnn_cell = layers.MRNNCell(input_size, hidden_size,
+                                         output_size, k)
 
-    def forward(self, inputs, h=None):
+    def forward(self, inputs, hidden_state=None):
         time_steps = inputs.size(0)
         batch_size = inputs.size(1)
         outputs = torch.Tensor(time_steps, batch_size, self.output_size)
-        for t in range(time_steps):
-            outputs[t, :], h = self.lmrnncell(inputs[t, :], h)
-        return outputs, h
+        for times in range(time_steps):
+            outputs[times, :], hidden_state = self.mrnn_cell(inputs[times, :],
+                                                             hidden_state)
+        return outputs, hidden_state
 
-# mLSTM with fixed d for time series prediction
-class MLSTM_fixD(nn.Module):
-    def __init__(self, input_size, hidden_size, K, output_size):
-        super(MLSTM_fixD, self).__init__()
+
+class MLSTMFixD(nn.Module):
+    """mLSTM with fixed d for time series prediction"""
+    def __init__(self, input_size, hidden_size, k, output_size):
+        super(MLSTMFixD, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.K = K
-        self.bd = Parameter(torch.Tensor(torch.zeros(1, hidden_size)), requires_grad=True)
+        self.k = k
+        self.d_values = Parameter(torch.Tensor(torch.zeros(1, hidden_size)),
+                                  requires_grad=True)
         self.output_size = output_size
-        self.mlstmcell = layers.MLSTM_fixD_cell(self.input_size, self.hidden_size, self.output_size, self.K)
+        self.mlstm_cell = layers.MLSTMFixDCell(self.input_size,
+                                               self.hidden_size,
+                                               self.output_size,
+                                               self.k)
         self.sigmoid = nn.Sigmoid()
 
-    def get_ws(self, d):
-        K = self.K
-        w = [1.] * (K + 1)
-        for i in range(K):
-            w[K - i - 1] = w[K - i] * (i - d) / (i + 1)
-        return torch.cat(w[0:K])
+    def get_w(self, d_values):
+        k = self.k
+        weights = [1.] * (k + 1)
+        for i in range(k):
+            weights[k - i - 1] = weights[k - i] * (i - d_values) / (i + 1)
+        return torch.cat(weights[0:k])
 
-    def get_wd(self, d):
-        w = torch.ones(self.K, 1, d.size(1), dtype=d.dtype, device=d.device)
-        batch_size = w.shape[1]
-        hidden_size = w.shape[2]
+    def get_wd(self, d_value):
+        weights = torch.ones(self.k, 1, d_value.size(1), dtype=d_value.dtype,
+                             device=d_value.device)
+        batch_size = weights.shape[1]
+        hidden_size = weights.shape[2]
         for sample in range(batch_size):
             for hidden in range(hidden_size):
-                w[:, sample, hidden] = self.get_ws(d[0, hidden].view([1]))
-        return w.squeeze(1)
+                weights[:, sample, hidden] = self.get_w(d_value[0, hidden].
+                                                        view([1]))
+        return weights.squeeze(1)
 
-    def forward(self, inputs, hx=None):
-        if hx == None:
+    def forward(self, inputs, hidden_states=None):
+        if hidden_states is None:
             hidden = None
-            hc = None
+            h_c = None
         else:
-            hidden = hx[0]
-            hc = hx[1]
+            hidden = hidden_states[0]
+            h_c = hidden_states[1]
         time_steps = inputs.shape[0]
         batch_size = inputs.shape[1]
-        outputs = torch.zeros(time_steps, batch_size, self.output_size, dtype=inputs.dtype, device=inputs.device)
-        self.d = 0.5 * F.sigmoid(self.bd)
-        wd = self.get_wd(self.d)
-        for t in range(time_steps):
-            outputs[t, :], hidden, hc = self.mlstmcell(inputs[t, :], hidden, hc, wd)
-        return outputs, (hidden, hc)
+        outputs = torch.zeros(time_steps, batch_size, self.output_size,
+                              dtype=inputs.dtype, device=inputs.device)
+        self.d_values_sigmoid = 0.5 * F.sigmoid(self.d_values)
+        weights_d = self.get_wd(self.d_values_sigmoid)
+        for times in range(time_steps):
+            outputs[times, :], hidden, h_c = self.mlstm_cell(inputs[times, :],
+                                                             hidden,
+                                                             h_c,
+                                                             weights_d)
+        return outputs, (hidden, h_c)
 
 
-# mLSTM with dynamic d for time series prediction
 class MLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, K, output_size):
+    """mLSTM with dynamic d for time series prediction"""
+    def __init__(self, input_size, hidden_size, k, output_size):
         super(MLSTM, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.K = K
+        self.k = k
         self.output_size = output_size
-        self.mlstmcell = layers.MLSTM_cell(self.input_size, self.hidden_size, self.K, self.output_size)
+        self.mlstm_cell = layers.MLSTMCell(self.input_size, self.hidden_size,
+                                           self.k, self.output_size)
 
-    def forward(self, inputs, hx=None):
-        if hx == None:
+    def forward(self, inputs, hidden_state=None):
+        if hidden_state is None:
             hidden = None
-            hc = None
-            d = None
+            h_c = None
+            d_values = None
         else:
-            hidden = hx[0]
-            hc = hx[1]
-            d = hx[2]
+            hidden = hidden_state[0]
+            h_c = hidden_state[1]
+            d_values = hidden_state[2]
         time_steps = inputs.shape[0]
         batch_size = inputs.shape[1]
-        outputs = torch.zeros(time_steps, batch_size, self.output_size, dtype=inputs.dtype, device=inputs.device)
-        for t in range(time_steps):
-            outputs[t, :], hidden, hc, d = self.mlstmcell(inputs[t, :], hidden, hc, d)
-        return outputs, (hidden, hc, d)
-
+        outputs = torch.zeros(time_steps, batch_size, self.output_size,
+                              dtype=inputs.dtype, device=inputs.device)
+        for times in range(time_steps):
+            outputs[times, :], hidden, h_c, d_values = \
+                self.mlstm_cell(inputs[times, :], hidden, h_c, d_values)
+        return outputs, (hidden, h_c, d_values)
